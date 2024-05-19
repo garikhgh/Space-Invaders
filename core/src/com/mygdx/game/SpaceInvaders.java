47d982dc472b94f2bee9c2b3181339a7e21e82c5
Garik Hakobyan <garik.hakobyan@daim.tech>
2024-05-19 20:33:26 +0400

    initial commit

--- core/src/com/mygdx/game/SpaceInvaders.java
+++ core/src/com/mygdx/game/SpaceInvaders.java
@@ -14,57 +14,57 @@ import com.mygdx.game.utils.TextureUtils;
 
 public class SpaceInvaders extends ApplicationAdapter {
 
-	int numWithAliens = 11;
-	int numHeightAliens = 5;
-	int spacingAliens = 48;
+    int numWithAliens = 11;
+    int numHeightAliens = 5;
+    int spacingAliens = 48;
 
-	int minXAlien;
-	int minYAlien;
-	int maxXAlien;
-	int maxYAlien;
+    int minXAlien;
+    int minYAlien;
+    int maxXAlien;
+    int maxYAlien;
 
-	int directionAliens = 1;
-	int speedAliens = 200;
+    int directionAliens = 1;
+    int speedAliens = 200;
 
-	int amountOfAliveAliens = 0;
-	SpriteBatch batch;
-	Texture img;
-	Texture imgBullet;
-	Texture alien;
-	Player player;
-	Alien[] alienList;
+    int amountOfAliveAliens = 0;
+    SpriteBatch batch;
+    Texture img;
+    Texture imgBullet;
+    Texture alien;
+    Player player;
+    Alien[] alienList;
 
-	Vector2 offsetAliens = new Vector2();
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = TextureUtils.readRocket("rocket.png", 75, 75);
-		imgBullet = TextureUtils.readRocket("bullet.png", 75, 75);
-		alien = TextureUtils.readRocket("alien.png", 48, 48);
-		player = new Player(img, imgBullet);
-		alienList = new Alien[numWithAliens * numHeightAliens];
-		int pos = 0;
-		for (int i = 0; i < numHeightAliens ; i++) {
-			for (int j = 0; j < numWithAliens; j++) {
+    Vector2 offsetAliens = new Vector2();
 
-				Vector2 position = new Vector2(j * spacingAliens , i * spacingAliens);
-				position.x += (float) Gdx.graphics.getWidth() / 2;
-				position.y += Gdx.graphics.getHeight();
-				position.x -= ((float)  numWithAliens / 2) * spacingAliens;
-				position.y -= numHeightAliens  * spacingAliens;
-				alienList[pos] = new Alien(position, alien);
-				pos++;
-			}
-		}
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = TextureUtils.readRocket("rocket.png", 75, 75);
+        imgBullet = TextureUtils.readRocket("bullet.png", 75, 75);
+        alien = TextureUtils.readRocket("alien.png", 48, 48);
+        player = new Player(img, imgBullet);
+        alienList = new Alien[numWithAliens * numHeightAliens];
+        int pos = 0;
+        for (int i = 0; i < numHeightAliens; i++) {
+            for (int j = 0; j < numWithAliens; j++) {
 
-	@Override
-	public void render () {
-		float deltaTime = Gdx.graphics.getDeltaTime();
-		ScreenUtils.clear(1, 1, 1, 1);
-		batch.begin();
-		player.drow(batch);
+                Vector2 position = new Vector2(j * spacingAliens, i * spacingAliens);
+                position.x += (float) Gdx.graphics.getWidth() / 2;
+                position.y += Gdx.graphics.getHeight();
+                position.x -= ((float) numWithAliens / 2) * spacingAliens;
+                position.y -= numHeightAliens * spacingAliens;
+                alienList[pos] = new Alien(position, alien);
+                pos++;
+            }
+        }
+    }
+
+    @Override
+    public void render() {
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        ScreenUtils.clear(1, 1, 1, 1);
+        batch.begin();
+        player.drow(batch);
 
         for (Alien value : alienList) {
             if (value.alive) {
@@ -77,61 +77,61 @@ public class SpaceInvaders extends ApplicationAdapter {
             }
 
         }
-		minXAlien = 10000;
-		minYAlien = 10000;
-		maxXAlien = 0;
-		maxYAlien = 0;
-		for (int i = 0; i <alienList.length; i++) {
+        minXAlien = 10000;
+        minYAlien = 10000;
+        maxXAlien = 0;
+        maxYAlien = 0;
+        for (int i = 0; i < alienList.length; i++) {
 
-			if (alienList[i].alive) {
-				int indexX = i%numWithAliens;
-				int indexY = i%numHeightAliens;
+            if (alienList[i].alive) {
+                int indexX = i % numWithAliens;
+                int indexY = i % numHeightAliens;
 
-				if (indexX > maxXAlien) maxXAlien = indexX;
-				if (indexX < minXAlien) minXAlien = indexX;
-				if (indexY > maxYAlien) maxYAlien = indexY;
-				if (indexY < minYAlien) minYAlien = indexY;
-				amountOfAliveAliens++;
-			}
+                if (indexX > maxXAlien) maxXAlien = indexX;
+                if (indexX < minXAlien) minXAlien = indexX;
+                if (indexY > maxYAlien) maxYAlien = indexY;
+                if (indexY < minYAlien) minYAlien = indexY;
+                amountOfAliveAliens++;
+            }
 
-		}
-		if (amountOfAliveAliens == 0) {
+        }
+        if (amountOfAliveAliens == 0) {
             for (Alien value : alienList) {
                 value.alive = true;
             }
-			offsetAliens = new Vector2(0, 0);
-		}
-		offsetAliens.x += directionAliens * deltaTime * speedAliens;
+            offsetAliens = new Vector2(0, 0);
+        }
+        offsetAliens.x += directionAliens * deltaTime * speedAliens;
 
-		if (alienList[maxXAlien].position.x >= Gdx.graphics.getWidth() - alienList[maxXAlien].sprite.getWidth()) {
-			directionAliens = -1;
-			offsetAliens.y -= alienList[0].sprite.getHeight()* 0.2f;
-			speedAliens+=10;
+        if (alienList[maxXAlien].position.x >= Gdx.graphics.getWidth() - alienList[maxXAlien].sprite.getWidth()) {
+            directionAliens = -1;
+            offsetAliens.y -= alienList[0].sprite.getHeight() * 0.2f;
+            speedAliens += 10;
 
-		}
-		if (alienList[minXAlien].position.x <=0){
-			directionAliens = 1;
-			offsetAliens.y -= alienList[0].sprite.getHeight()* 0.2f;
-			speedAliens+=10;
-		}
+        }
+        if (alienList[minXAlien].position.x <= 0) {
+            directionAliens = 1;
+            offsetAliens.y -= alienList[0].sprite.getHeight() * 0.2f;
+            speedAliens += 10;
+        }
 
         for (Alien alien : alienList) {
-			alien.position = new Vector2(alien.initialPosition.x + offsetAliens.x, alien.initialPosition.y + offsetAliens.y);
+            alien.position = new Vector2(alien.initialPosition.x + offsetAliens.x, alien.initialPosition.y + offsetAliens.y);
             if (alien.alive) {
                 alien.draw(batch);
-				if (alien.sprite.getBoundingRectangle().overlaps(player.sprite.getBoundingRectangle())){
-					Gdx.app.exit();
-				}
+                if (alien.sprite.getBoundingRectangle().overlaps(player.sprite.getBoundingRectangle())) {
+                    Gdx.app.exit();
+                }
             }
         }
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+        batch.end();
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        img.dispose();
+    }
 
 
 }
